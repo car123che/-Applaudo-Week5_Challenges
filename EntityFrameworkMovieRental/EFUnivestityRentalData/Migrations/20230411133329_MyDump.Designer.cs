98bcd4e2117f779724b08195fd3227e0632f256d
Carlos Ché <3013216470101@ingenieria.usac.edu.gt>
2023-04-11 07:41:50 -0600

    DB Dump

--- EntityFrameworkMovieRental/EFUnivestityRentalData/Migrations/20230411133329_MyDump.Designer.cs
+++ EntityFrameworkMovieRental/EFUnivestityRentalData/Migrations/20230411133329_MyDump.Designer.cs
@@ -4,6 +4,7 @@ using EFUnivestityRentalData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFUnivestityRentalData.Migrations
 {
     [DbContext(typeof(MovieRentalContext))]
-    partial class MovieRentalContextModelSnapshot : ModelSnapshot
+    [Migration("20230411133329_MyDump")]
+    partial class MyDump
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
