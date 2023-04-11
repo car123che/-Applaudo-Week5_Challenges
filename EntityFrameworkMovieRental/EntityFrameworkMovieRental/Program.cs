c5962e81297bec485a5682ed2bbef69e883d1f95
Carlos Ché <3013216470101@ingenieria.usac.edu.gt>
2023-04-11 07:22:55 -0600

    First Commit

---
+++ EntityFrameworkMovieRental/EntityFrameworkMovieRental/Program.cs
@@ -0,0 +1,30 @@
+﻿using EFMovieRentalDomain;
+using EFUnivestityRentalData;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkMovieRental
+{
+    internal class Program
+    {
+
+        private static MovieRentalContext context = new MovieRentalContext();
+
+        static async Task Main(string[] args)
+        {
+            await QueryFilter();
+
+            Console.ReadLine();
+        }
+
+        private static async Task QueryFilter()
+        {
+            var movies = await context.Movies.Where(e => e.Title.Contains("n")).ToListAsync();
+
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+        }
+    }
+}
